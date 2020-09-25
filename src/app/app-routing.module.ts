9e5a42d2cbab987b836f54c869ac47cbb2da95be
George Babă <xsuve@Georges-MacBook-Pro.local>
2020-09-25 16:45:04 +0300

    Up to date online files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactAddComponent } from './contact-add/contact-add.component';
@@ -11,6 +12,11 @@ import { ContactEditComponent } from './contact-edit/contact-edit.component';
 import { ContactViewComponent } from './contact-view/contact-view.component';
 import { CompaniesComponent } from './companies/companies.component';
 import { CompanyAddComponent } from './company-add/company-add.component';
+import { CompanyEditComponent } from './company-edit/company-edit.component';
+import { PipelinesComponent } from './pipelines/pipelines.component';
+import { PipelineAddComponent } from './pipeline-add/pipeline-add.component';
+import { PipelineViewComponent } from './pipeline-view/pipeline-view.component';
+import { SettingsComponent } from './settings/settings.component';
 
 import { AuthenticationGuard } from './guards/authentication.guard';
 import { LoggedInGuard } from './guards/loggedin.guard';
@@ -22,10 +28,15 @@ const routes: Routes = [
   { path: 'dashboard', pathMatch: 'full', component: DashboardComponent, canActivate: [AuthenticationGuard] },
   { path: 'contacts', pathMatch: 'full', component: ContactsComponent, canActivate: [AuthenticationGuard] },
   { path: 'contacts/add', pathMatch: 'full', component: ContactAddComponent, canActivate: [AuthenticationGuard] },
-  { path: 'contacts/edit/:id', pathMatch: 'full', component: ContactEditComponent, canActivate: [AuthenticationGuard] },
+  { path: 'contact/edit/:id', pathMatch: 'full', component: ContactEditComponent, canActivate: [AuthenticationGuard] },
   { path: 'contact/:id', pathMatch: 'full', component: ContactViewComponent, canActivate: [AuthenticationGuard] },
   { path: 'companies', pathMatch: 'full', component: CompaniesComponent, canActivate: [AuthenticationGuard] },
   { path: 'companies/add', pathMatch: 'full', component: CompanyAddComponent, canActivate: [AuthenticationGuard] },
+  { path: 'company/edit/:id', pathMatch: 'full', component: CompanyEditComponent, canActivate: [AuthenticationGuard] },
+  { path: 'pipelines', pathMatch: 'full', component: PipelinesComponent, canActivate: [AuthenticationGuard] },
+  { path: 'pipelines/add', pathMatch: 'full', component: PipelineAddComponent, canActivate: [AuthenticationGuard] },
+  { path: 'pipeline/:id', pathMatch: 'full', component: PipelineViewComponent, canActivate: [AuthenticationGuard] },
+  { path: 'settings', pathMatch: 'full', component: SettingsComponent, canActivate: [AuthenticationGuard] },
   { path: '**', redirectTo: '/' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,82 +1,98 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { Platform } from '@angular/cdk/platform';
+import { ViewportRuler, ScrollDispatcher } from '@angular/cdk/scrolling';
+import { DragDropModule, DragDropRegistry } from '@angular/cdk/drag-drop';
 
 import { FullCalendarModule } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-
-import { UserService } from './services/user.service';
-import { UtilsService } from './services/utils.service';
-import { SearchContactsPipe } from './pipes/search-contacts.pipe';
-
-import { AuthenticationGuard } from './guards/authentication.guard';
-import { LoggedInGuard } from './guards/loggedin.guard';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { NavLandingPageComponent } from './nav-landing-page/nav-landing-page.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { NavComponent } from './nav/nav.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { ContactAddComponent } from './contact-add/contact-add.component';
-import { CompaniesComponent } from './companies/companies.component';
-import { CompanyAddComponent } from './company-add/company-add.component';
-import { ContactEditComponent } from './contact-edit/contact-edit.component';
-import { ContactViewComponent } from './contact-view/contact-view.component';
-import { SiderComponent } from './sider/sider.component';
-
-registerLocaleData(en);
-
 FullCalendarModule.registerPlugins([
   dayGridPlugin,
   timeGridPlugin,
   interactionPlugin
 ]);
 
+import { UserService } from './services/user.service';
+
+import { AuthenticationGuard } from './guards/authentication.guard';
+import { LoggedInGuard } from './guards/loggedin.guard';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavLandingPageComponent } from './nav-landing-page/nav-landing-page.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { SiderComponent } from './sider/sider.component';
+import { NavComponent } from './nav/nav.component';
+import { ContactAddComponent } from './contact-add/contact-add.component';
+import { ContactViewComponent } from './contact-view/contact-view.component';
+import { ContactEditComponent } from './contact-edit/contact-edit.component';
+import { SearchContactsPipe } from './pipes/search-contacts.pipe';
+import { CompaniesComponent } from './companies/companies.component';
+import { CompanyAddComponent } from './company-add/company-add.component';
+import { CompanyEditComponent } from './company-edit/company-edit.component';
+import { CompanyViewComponent } from './company-view/company-view.component';
+import { PipelinesComponent } from './pipelines/pipelines.component';
+import { PipelineAddComponent } from './pipeline-add/pipeline-add.component';
+import { PipelineViewComponent } from './pipeline-view/pipeline-view.component';
+import { SettingsComponent } from './settings/settings.component';
+
+registerLocaleData(en);
+
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
+    ContactsComponent,
     LandingPageComponent,
     NavLandingPageComponent,
-    SignupComponent,
     LoginComponent,
-    DashboardComponent,
+    SignupComponent,
+    SiderComponent,
     NavComponent,
-    ContactsComponent,
     ContactAddComponent,
+    ContactViewComponent,
+    ContactEditComponent,
+    SearchContactsPipe,
     CompaniesComponent,
     CompanyAddComponent,
-    ContactEditComponent,
-    ContactViewComponent,
-    SiderComponent,
-    SearchContactsPipe
+    CompanyEditComponent,
+    CompanyViewComponent,
+    PipelinesComponent,
+    PipelineAddComponent,
+    PipelineViewComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
-    NgZorroAntdModule,
+    DragDropModule,
     FullCalendarModule,
-    HttpClientModule,
     AppRoutingModule,
+    NgZorroAntdModule,
     FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule
   ],
   providers: [
     UserService,
-    UtilsService,
     AuthenticationGuard,
     LoggedInGuard,
-    SearchContactsPipe,
+    Platform,
+    ViewportRuler,
+    DragDropRegistry,
+    ScrollDispatcher,
     { provide: NZ_I18N, useValue: en_US }
   ],
   bootstrap: [AppComponent]
