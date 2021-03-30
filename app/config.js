49750cf55a812ad516d9c3907aadd951d367d103
xsuve <georgebaba99@yahoo.com>
2021-03-30 20:37:50 +0300

    Fix

---
+++ app/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  url: 'http://localhost:4200',
+  server_url: 'http://localhost:8000',
+  private_key: '',
+  public_key: '',
+  email_host: '',
+  email_user: '',
+  email_pass: ''
+};
